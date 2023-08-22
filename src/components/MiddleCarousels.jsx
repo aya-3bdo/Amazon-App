c162434289f018bb7f1ad14d30613e65d464efa3
aya-3bdo <aya.aghany@yahoo.com>
2023-08-22 04:34:30 +0300

    Working on the project

--- src/components/MiddleCarousels.jsx
+++ src/components/MiddleCarousels.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Slider from "react-slick";
 import "/node_modules/slick-carousel/slick/slick.css";
 import "/node_modules/slick-carousel/slick/slick-theme.css";
-import '/src/styling/MiddleCarousels.css'
-import {MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft} from 'react-icons/md'
-
+import "/src/styling/MiddleCarousels.css";
+import {
+  MdOutlineKeyboardArrowRight,
+  MdOutlineKeyboardArrowLeft,
+} from "react-icons/md";
 
 export default function MiddleCarousels() {
   let settings = {
@@ -16,56 +18,172 @@ export default function MiddleCarousels() {
     autoplay: true,
   };
   return (
-    <div className="middle-carousels-container container-xl mt-4">
+    <div className="middle-carousels-container container-xl">
       {/*  First Carousel */}
       <div className="first-carousel slider-container px-1 position-relative">
-        <div className="slider-title pt-1">Shop top picks in sports
+        <div className="slider-title pt-1">
+          Shop top picks in sports
           <span className="ms-3">explore more</span>
         </div>
-          <span className="left-arrow"><MdOutlineKeyboardArrowLeft/></span>
-          <span className="right-arrow"><MdOutlineKeyboardArrowRight/></span>
-        <Slider {...settings} className='py-4'>
-
-        <img  src="/src/assets/imgs/4four-carousel-1.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-2.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-3.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-4.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-5.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-6.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-7.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-8.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-9.jpg" />
-        <img  src="/src/assets/imgs/4four-carousel-10.jpg" />
+        <span className="left-arrow">
+          <MdOutlineKeyboardArrowLeft />
+        </span>
+        <span className="right-arrow">
+          <MdOutlineKeyboardArrowRight />
+        </span>
+        <Slider {...settings} className="py-4">
+          <img src="/src/assets/imgs/4four-carousel-1.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-2.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-3.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-4.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-5.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-6.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-7.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-8.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-9.jpg" />
+          <img src="/src/assets/imgs/4four-carousel-10.jpg" />
         </Slider>
-        
-      </div> 
-
+      </div>
 
       {/* Second Carousel */}
       <div className="second-carousel slider-container px-1 position-relative mt-3 ">
-        <div className="slider-title mb-1 pt-1">Shop the Budget Store
+        <div className="slider-title mb-1 pt-1 ">
+          Shop the Budget Store
           <span className="ms-3">shop all deals</span>
         </div>
-          <span className="left-arrow"><MdOutlineKeyboardArrowLeft/></span>
-          <span className="right-arrow"><MdOutlineKeyboardArrowRight/></span>
-        <Slider {...settings} className=" py-1">
-
-        <img  src="/src/assets/imgs/3thir-carousel-1.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-2.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-3.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-4.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-5.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-6.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-7.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-8.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-9.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-10.jpg" />
-        <img  src="/src/assets/imgs/3thir-carousel-11.jpg" />
+        <span className="left-arrow">
+          <MdOutlineKeyboardArrowLeft />
+        </span>
+        <span className="right-arrow">
+          <MdOutlineKeyboardArrowRight />
+        </span>
+        <Slider {...settings} className=" py-1 ">
+          <img src="/src/assets/imgs/3thir-carousel-1.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-2.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-3.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-4.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-5.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-6.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-7.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-8.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-9.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-10.jpg" />
+          <img src="/src/assets/imgs/3thir-carousel-11.jpg" />
         </Slider>
-        
       </div>
 
+      {/*  Third Carousel */}
+
+      <div className="third-carousel slider-container px-1 position-relative mt-3">
+        <div className="slider-title mb-1 pt-1 ">
+          Shop Today's Deals
+          <span className="ms-3">shop all deals</span>
+        </div>
+        <span className="left-arrow">
+          <MdOutlineKeyboardArrowLeft />
+        </span>
+        <span className="right-arrow">
+          <MdOutlineKeyboardArrowRight />
+        </span>
+        <Slider {...settings} className="py-2">
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-1.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-2.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-3.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-4.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-5.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-6.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-7.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-8.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-9.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-10.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-11.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+          <div className="img-container">
+            <img src="/src/assets/imgs/4th-carousel-12.jpg" />
+            <div className="description">
+              up to 15% off Deal <span>
+                buy now and save
+              </span>
+            </div>
+          </div>
+        </Slider>
+      </div>
     </div>
-      
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import BigCarousel from "./BigCarousel";
 import MiddleCarousels from "./MiddleCarousels";
+import FirstCarousels from "./FirstCarousels";
 
 const Products = () => {
   return (
-    <>
+    <div className="Products m-0 p-0 position-relative">
       {/* Big carousel & first 2 Carousels */}
-      <BigCarousel/>
+      <BigCarousel />
+      <FirstCarousels/>
       <MiddleCarousels/>
       
       {/*  Second Carousels */}
       
 
-    </>
+    </div>
      
       
   );
